612c4f5d7c7189c07fdb7576a51584afd79372c1
GeorgeSmileyface <104126452+GeorgeSmileyface@users.noreply.github.com>
2025-03-31 08:48:30 +0200

    added main code

--- Arv/Program.cs
+++ Arv/Program.cs
@@ -1,2 +1,12 @@
-﻿
+﻿using Arv;
+
+Doctor doctor = new Doctor("Dermatologist");
+
+Person person = new Person("Peter", 12345678, "ojfi@fj.dk");
+
+Patient patient = new Patient("Peter", 12345678, "ojfi@fj.dk", "Penecillin");
+
+List<Person> persons = new List<Person>();
+persons.Add(person);
+persons.Add(patient);
 
